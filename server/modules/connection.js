70e4de8e8de89e28ea154341bff4d9c06cd19a64
rohran01 <anthonytrohr@gmail.com>
2016-08-16 10:15:10 -0500

    swaps delete and edit icons

--- server/modules/connection.js
+++ server/modules/connection.js
@@ -3,8 +3,8 @@ var connectionString = '';
 if(process.env.DATABASE_URL != undefined) {
     connectionString = process.env.DATABASE_URL + 'ssl';
 } else {
-    // connectionString = 'postgres://localhost:5432/labresults';                                 //Testing at home
-    connectionString = 'postgres://postgres:retro1invert@localhost:5432/labresults';              //Testing at work
+    connectionString = 'postgres://localhost:5432/labresults';                                 //Testing at home
+    // connectionString = 'postgres://postgres:retro1invert@localhost:5432/labresults';              //Testing at work
 }
 
 module.exports = connectionString;
